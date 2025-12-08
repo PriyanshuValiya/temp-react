f8a2fa5c028d23f30f409cb45b2b2e32c7a75476
PriyanshuValiya <147643182+PriyanshuValiya@users.noreply.github.com>
2025-12-09 00:57:17 +0530

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const [userData, setUserData] = useState({
     <>
       <div>
         <h1>
-          Hello Dayaaaaa, {userData.firstName} {userData.lastName} !
+          Hello Dayaaaaaa, {userData.firstName} {userData.lastName} !
         </h1>
         <div>
           <input
